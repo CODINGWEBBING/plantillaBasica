73bb9b2a6638aaf536cad87f39097b8bcc7b9aca
Enzo <Enzo.Santoro@es.ddb.com>
2018-07-16 14:08:10 +0200

    lunes

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp =                require('gulp'),
+      sass =                require('gulp-sass'),
+      autoprefixer =        require('gulp-autoprefixer');
+
+gulp.task('sass', ()=>    
+    gulp.src('./scss/*.scss')      
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css'))
+);
