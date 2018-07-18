9fcf725c9fb157875bcae9c9495135fcbf7c7a85
Enzo <Enzo.Santoro@es.ddb.com>
2018-07-18 13:49:27 +0200

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 const gulp =                require('gulp'),
       sass =                require('gulp-sass'),
-      autoprefixer =        require('gulp-autoprefixer');
+      autoprefixer =        require('gulp-autoprefixer'),
+      watch =               require('gulp-watch');
 
-gulp.task('sass', ()=>    
+gulp.task('sass', ()=> { 
     gulp.src('./scss/*.scss')      
         .pipe(sass({
             outputStyle: 'expanded'
+            //outputStyle: 'compressed'
         }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('./css'))
-);
+ });
+
+
+gulp.task('watch', ['sass'], ()=>  { 
+  gulp.watch('./scss/*.scss', ['sass']); 
+  // Other watchers
+ });
